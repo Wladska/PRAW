611e3383e21ff3d2c289b3210edc28646e5091b0
Popularkiya <kingawladzinsk@gmail.com>
2023-11-21 20:47:29 +0100

    rmove redundant code

--- mergesort.cu
+++ mergesort.cu
@@ -8,38 +8,18 @@
 #define MAX_DISTRIBUTION 10000
 
 #define INITIAL_ARRAY_SIZE 20
-#define HEAD_PRINT_INPUT_DATA 20
-
-/// only the x axis y and z are set to 1
-#define KERNELS_NUM 1 // unused
-#define BLOCKS_NUM 1 // unused
+#define HEAD_PRINT_INPUT_DATA INITIAL_ARRAY_SIZE
 #define THREADS_NUM INITIAL_ARRAY_SIZE
 
 __device__ unsigned int calcSelfGlobalIndex(){
     return threadIdx.x + blockIdx.x * blockDim.x;
 }
 
-
 __device__ void copy(int* source, int* destination, int startIndex, int numberOfElementsToCopy) {
     for (int i = 0; i < numberOfElementsToCopy; i++) {
         destination[startIndex + i] = source[startIndex + i];
     }
 }
-
-/*
-Input:          8 3 1 9 1 2 7 5 9 3 6 4 2 0 2 5
-Threads: |    t1    |    t2    |    t3    |    t4    |
-         | 8 3 1 9  | 1 2 7 5  | 9 3 6 4  | 2 0 2 5  |
-         |  38 19   |  12 57   |  39 46   |  02 25   |
-         |   1398   |   1257   |   3469   |   0225   |
-         +----------+----------+----------+----------+
-         |          t1         |          t3         |
-         |       11235789      |       02234569      |
-         +---------------------+---------------------+
-         |                     t1                    |
-         |      0 1 1 2 2 2 3 3 4 5 5 6 7 8 9 9      |
-
-*/
 
 __device__ void merge(int startIdx, int endIdx, int* inputData, int* outputData) {
     int middleIdx = (startIdx + endIdx) / 2;
@@ -66,7 +46,7 @@ __global__ void mergeSortGPUBasic(int* input, int* midMerge, int size) {
     __syncthreads () ;
     
     for (unsigned int offset = 1; offset < blockDim.x; offset *= 2) {
-        if ( localThreadId % (2* offset) == 0) {
+        if ( localThreadId % (2 * offset) == 0) {
             merge(localThreadId, localThreadId + offset, sharedData, midMerge);
             copy(midMerge, sharedData, localThreadId, offset);
         }
@@ -89,22 +69,17 @@ int* generateRandomInput(int size) {
 }
 
 int main(int argc, char *argv[]) {
-    // std::cout << "Usage: " << argv[0] << " <array_size> <generated_input_head_number>" << std::endl;
-
     int generatedInputHead = HEAD_PRINT_INPUT_DATA;
     int initialArraySize = INITIAL_ARRAY_SIZE;
 
-    if (argc >= 2) {
-        initialArraySize = std::atoi(argv[1]);
-        if (argc > 2) {
-            generatedInputHead = std::atoi(argv[2]);
-            if (generatedInputHead > initialArraySize) {
-                generatedInputHead = initialArraySize;
-            }
-        }
-    }
-
     int* randomNumbers = generateRandomInput(initialArraySize);
+
+    // Print the input array
+    for (int i = 0; i < generatedInputHead; i++) {
+        std::cout << randomNumbers[i] << " ";
+    }
+    std::cout << std::endl;
+
     int *inputData, *outputData;
 
     // Allocate memory on GPU
@@ -122,12 +97,6 @@ int main(int argc, char *argv[]) {
 
     int result[initialArraySize];
     cudaMemcpy(result, outputData, initialArraySize * sizeof(int), cudaMemcpyDeviceToHost);
-
-    // Print the input array
-    for (int i = 0; i < generatedInputHead; i++) {
-        std::cout << randomNumbers[i] << " ";
-    }
-    std::cout << std::endl;
 
     // Print the sorted array
     for (int i = 0; i < initialArraySize; i++) {
